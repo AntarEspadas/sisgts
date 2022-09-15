c7d5271437056af7ff605cbe5e9103a62c6b2819
Naratna <antar.espadas@hotmail.com>
2022-09-15 03:10:31 -0500

    Update Filtro javadoc

--- src/main/java/mx/uam/ayd/proyecto/util/Filtro.java
+++ src/main/java/mx/uam/ayd/proyecto/util/Filtro.java
@@ -2,6 +2,12 @@ package mx.uam.ayd.proyecto.util;
 
 import lombok.Data;
 
+/**
+ * Clase de utilidad que ayuda al repositorio de citas a filtrar la lista de todas las citas
+ * según una lista dinámica de filtros
+ *
+ * @author Antar Espadas
+ */
 @Data
 public class Filtro {
 
