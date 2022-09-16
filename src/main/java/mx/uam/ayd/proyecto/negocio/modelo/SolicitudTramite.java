e239cbd94bbba11387ed83b31ddd338bd252498f
Adolfo MD <adolf.md.uam@gmail.com>
2022-09-16 17:09:07 -0500

    Corrección atributos entidad SolicitudTramite finalizados

--- src/main/java/mx/uam/ayd/proyecto/negocio/modelo/SolicitudTramite.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/modelo/SolicitudTramite.java
@@ -4,10 +4,16 @@ import lombok.Data;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 
 @Data
@@ -18,11 +24,17 @@ public class SolicitudTramite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idSolicitud;
 
+    @ManyToOne(targetEntity = TipoTramite.class, fetch = FetchType.EAGER)
+    private long tipoTramite;
+
+    @OneToMany(targetEntity = Documento.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = false)
     private List <Documento> requisitos;
 
     private String estado;
 
+	@OneToOne(targetEntity = Documento.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = false)
     private Documento acuse;
 
+    @OneToOne(targetEntity = Documento.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = false)
     private Documento documentoTramite;
 }
