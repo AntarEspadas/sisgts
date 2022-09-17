240e5919b83ba0ed9b1d8c910a2dfbfbb196f5fc
Naratna <antar.espadas@hotmail.com>
2022-09-17 11:37:10 -0500

    Actualiza RepositoryCita  - Añade método findByFechaAndByHora

--- src/main/java/mx/uam/ayd/proyecto/datos/RepositoryCita.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/RepositoryCita.java
@@ -4,7 +4,9 @@ import mx.uam.ayd.proyecto.negocio.modelo.Cita;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -17,4 +19,14 @@ public interface RepositoryCita extends CrudRepository<Cita, Long> {
      * Regrese todas las citas después de cierta fecha
      */
     Iterable<Cita> findByFechaAfter(LocalDate fecha);
+
+    /**
+     * Encuentra la una cita dada una fecha y hora
+     *
+     * @param fecha la fecha de la cita
+     * @param hora la hora de la cita
+     *
+     * @return null si la cita no se encontró
+     */
+    Cita findByFechaAndByHora(LocalDate fecha, LocalTime hora);
 }
