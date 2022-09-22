955874db75f4cd1cb1c7270c3b4039b07c6532da
Naratna <antar.espadas@hotmail.com>
2022-09-21 23:59:06 -0500

    Actualiza layout de VentanaInfoCitas

--- src/main/java/mx/uam/ayd/proyecto/presentacion/agendarCita/VentanaInfoCitas.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/agendarCita/VentanaInfoCitas.java
@@ -43,50 +43,51 @@ public class VentanaInfoCitas extends Pantalla {
 	public VentanaInfoCitas() {
 		setBounds(new Rectangle(100, 100, 500, 400));
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{36, 60, 49, 321, 0};
-		gridBagLayout.rowHeights = new int[]{37, 17, 30, 17, 17, 164, 27, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[]{36, 64, 321, 58, 0, 0};
+		gridBagLayout.rowHeights = new int[]{37, 17, 30, 17, 17, 164, 27, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
-		
+
 		JLabel lblHorarios = new JLabel("Horarios");
 		GridBagConstraints gbc_lblHorarios = new GridBagConstraints();
 		gbc_lblHorarios.anchor = GridBagConstraints.NORTH;
-		gbc_lblHorarios.insets = new Insets(0, 0, 5, 0);
-		gbc_lblHorarios.gridx = 3;
+		gbc_lblHorarios.insets = new Insets(0, 0, 5, 5);
+		gbc_lblHorarios.gridx = 2;
 		gbc_lblHorarios.gridy = 1;
 		add(lblHorarios, gbc_lblHorarios);
-		
+
 		JLabel lblHorarios2 = new JLabel("Lunes a Viernes de 10:00 a 18:00");
 		GridBagConstraints gbc_lblHorarios2 = new GridBagConstraints();
-		gbc_lblHorarios2.anchor = GridBagConstraints.NORTHWEST;
-		gbc_lblHorarios2.insets = new Insets(0, 0, 5, 0);
-		gbc_lblHorarios2.gridx = 3;
+		gbc_lblHorarios2.anchor = GridBagConstraints.NORTH;
+		gbc_lblHorarios2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblHorarios2.gridx = 2;
 		gbc_lblHorarios2.gridy = 3;
 		add(lblHorarios2, gbc_lblHorarios2);
 		
-		JLabel lblSusCitas = new JLabel("Sus citas");
-		GridBagConstraints gbc_lblSusCitas = new GridBagConstraints();
-		gbc_lblSusCitas.anchor = GridBagConstraints.NORTH;
-		gbc_lblSusCitas.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblSusCitas.insets = new Insets(0, 0, 5, 5);
-		gbc_lblSusCitas.gridx = 1;
-		gbc_lblSusCitas.gridy = 4;
-		add(lblSusCitas, gbc_lblSusCitas);
-		
+				JLabel lblSusCitas = new JLabel("Sus citas");
+				GridBagConstraints gbc_lblSusCitas = new GridBagConstraints();
+				gbc_lblSusCitas.anchor = GridBagConstraints.NORTH;
+				gbc_lblSusCitas.fill = GridBagConstraints.HORIZONTAL;
+				gbc_lblSusCitas.insets = new Insets(0, 0, 5, 5);
+				gbc_lblSusCitas.gridx = 1;
+				gbc_lblSusCitas.gridy = 4;
+				add(lblSusCitas, gbc_lblSusCitas);
+
 		listaCitas = new JList<>();
 		GridBagConstraints gbc_listaCitas = new GridBagConstraints();
-		gbc_listaCitas.fill = GridBagConstraints.BOTH;
-		gbc_listaCitas.insets = new Insets(0, 0, 5, 0);
 		gbc_listaCitas.gridwidth = 3;
+		gbc_listaCitas.fill = GridBagConstraints.BOTH;
+		gbc_listaCitas.insets = new Insets(0, 0, 5, 5);
 		gbc_listaCitas.gridx = 1;
 		gbc_listaCitas.gridy = 5;
 		add(listaCitas, gbc_listaCitas);
-		
+
 		JButton btnAgendarCita = new JButton("Agendar Cita");
 		GridBagConstraints gbc_btnAgendarCita = new GridBagConstraints();
-		gbc_btnAgendarCita.anchor = GridBagConstraints.NORTHWEST;
-		gbc_btnAgendarCita.gridx = 3;
+		gbc_btnAgendarCita.insets = new Insets(0, 0, 5, 5);
+		gbc_btnAgendarCita.anchor = GridBagConstraints.NORTH;
+		gbc_btnAgendarCita.gridx = 2;
 		gbc_btnAgendarCita.gridy = 6;
 		add(btnAgendarCita, gbc_btnAgendarCita);
 		
