7615062f25ce2da65721e65e9efa73cb608db08a
Adolfo MD <adolf.md.uam@gmail.com>
2022-09-22 16:24:11 -0500

    Añadido servicios de Documento, SolicitudTramite, TipoTramite

---
+++ src/main/java/mx/uam/ayd/proyecto/negocio/ServicioSolicitudTramite.java
@@ -0,0 +1,21 @@
+package mx.uam.ayd.proyecto.negocio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mx.uam.ayd.proyecto.datos.SolicitudTramiteRepository;
+import mx.uam.ayd.proyecto.negocio.modelo.SolicitudTramite;
+
+@Service
+public class ServicioSolicitudTramite {
+
+    @Autowired
+    private SolicitudTramiteRepository solicitudTramiteRepository;
+
+    public List<SolicitudTramite> findByEstadoNotFinalizado(){
+        return solicitudTramiteRepository.findByEstadoNot("Finalizado");
+    };
+    
+}
---
+++ src/main/java/mx/uam/ayd/proyecto/negocio/ServicioTipoTramite.java
@@ -0,0 +1,8 @@
+package mx.uam.ayd.proyecto.negocio;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioTipoTramite {
+    
+}
